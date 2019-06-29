cf00784a0e4edf592b1de400acc6c4e09ce973da
amit2929 <amit0012@gmail.com>
2019-06-29 17:00:23 +0530

    added the app.jsx and removed app.js

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { directive } from "@babel/types";
+
+function App() {
+  return <div>This is the react app for first change</div>;
+}
+
+export default App;
